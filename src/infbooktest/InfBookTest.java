35e4b81c62d2ff352302e2344f5f326a26396e0a
Tomasz <Tomasz@DESKTOP-0IHJEAU.lan>
2019-01-30 09:34:04 +0100

    First commit

---
+++ src/infbooktest/InfBookTest.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package infbooktest;
+
+/**
+ *
+ * @author Tomasz
+ */
+import java.sql.*;
+import javax.swing.JOptionPane;
+
+/**
+ * @author Tomasz Baslyk
+ */
+public class InfBookTest {
+
+    private static Connection connection;
+
+    public static void main(String[] args) {
+
+        try {
+            // Försöker ansluta till databasen
+            connection = DriverManager.getConnection("jdbc:firebirdsql://10.22.15.91:3050/c:/db/hogdb.fdb", "SYSDBA", "masterkey");
+
+            // Fångar fel med databasen och printar ut felmeddelande
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, "Något gick fel med databasen!");
+            System.out.println("Internt felmeddelande: " + e.getMessage());
+        }
+
+    }
+}
